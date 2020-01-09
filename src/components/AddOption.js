4eab2b446176f637ab70d431e4fa9eb4820099f6
Chidimma Okafor <chidimma.okafor.c@gmail.com>
2020-01-09 22:21:47 +0100

    modified .gitignore file

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -2,7 +2,7 @@ import React from 'react';
 class AddOption extends React.Component {
   state = {
     error: undefined
-  }
+  };
   /*
     i installed a new babel package
       babel-plugin-transform-class-properties
@@ -21,10 +21,10 @@ class AddOption extends React.Component {
 
     -b ensures that the "this" key word is bound to every method declared in a class whether event handlers or regular functions
 
-      -b is achieved by changing the methods to be arrow functions as shown below for onFormSubmit
+      -b is achieved by changing the methods to be arrow functions as shown below for onFormSubmit without the 'const' keyword
   */
-  
-  onFormSubmit = (event) => {
+
+  onFormSubmit = event => {
     event.preventDefault();
     console.dir(event.target, 'target');
     console.log(event.target.elements.option.value, 'value');
@@ -35,20 +35,20 @@ class AddOption extends React.Component {
     if (!error) {
       event.target.elements.option.value = '';
     }
-  }
+  };
   render() {
     return (
       <div>
-        {
-          this.state.error && <p className="add-option-error">{this.state.error}</p>
-        }
-        <form onSubmit={this.onFormSubmit} className="add-option">
-          <input name="option" type="text" className="add-option-input"/>
-          <button className="button">Add new Option</button>
+        {this.state.error && (
+          <p className='add-option-error'>{this.state.error}</p>
+        )}
+        <form onSubmit={this.onFormSubmit} className='add-option'>
+          <input name='option' type='text' className='add-option-input' />
+          <button className='button'>Add new Option</button>
         </form>
       </div>
     );
-  };
-};
+  }
+}
 
 export default AddOption;
